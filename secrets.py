f0cfcf73485775b3215b0532e9e9953d7ef48c3c
koasare <opare.asare@gmail.com>
2020-07-18 20:00:10 -0400

    Final version of offline pet planter

---
+++ secrets.py
@@ -0,0 +1,10 @@
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+
+secrets = {
+    'ssid' : 'Accra Central',
+    'password' : "hogbetsotsoshawama#3",
+    'timezone' : "America/New_York",
+    'aio_username' : "koasare",
+    'aio_key' : "aio_xfKK12Si1KLztnPLtVsKr7FVRPjT",
+    }
